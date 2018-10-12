2ab7029c84f2014031377cf6880c9bc5edf8cb5c
boyerk <35824068+boyerk@users.noreply.github.com>
2018-10-12 12:39:36 -0700

    added marker scripts

---
+++ Assets/Resources/ARmanager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARmanager : MonoBehaviour {
+
+
+
+    public static ARmanager _instance = null;
+    public static ARmanager Instance { get { return _instance;}}
+    // Use this for initialization
+
+    void Awake()
+    {
+       if(_instance == null)
+        {
+            _instance = this;
+        } else if (_instance != this)
+        {
+            Destroy(this);
+        }
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void SomeMarkerMessage(string OBJECT)
+    {
+        Debug.Log(OBJECT);
+    }
+}
---
+++ Assets/Resources/MarkerDetected.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MarkerDetected : MonoBehaviour, ITrackableEventHandler {
+
+
+    private TrackableBehaviour myBehaviour;
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            Debug.Log("Hey! I found:" + gameObject.name);
+            ARmanager.Instance.SomeMarkerMessage(gameObject.name);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        myBehaviour = GetComponent<TrackableBehaviour>();
+        if (myBehaviour)
+        {
+            myBehaviour.RegisterTrackableEventHandler(this);
+           
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
