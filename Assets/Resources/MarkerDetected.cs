0431f30b1eb0ac7ed039b6c1f1ca80c6adac146e
boyerk <35824068+boyerk@users.noreply.github.com>
2018-10-18 21:14:03 -0700

    gud project bby

--- Assets/Resources/MarkerDetected.cs
+++ Assets/Resources/MarkerDetected.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 
 public class MarkerDetected : MonoBehaviour, ITrackableEventHandler {
-
-
+    
+    bool btnEnabled = false;
+    bool buttonWasClicked = false;
+    public GameObject btnGo;
+    public GameObject particlerainButton;
+    public GameObject occlusionPlane;
     private TrackableBehaviour myBehaviour;
 
     public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
@@ -14,7 +19,25 @@ public class MarkerDetected : MonoBehaviour, ITrackableEventHandler {
         {
             Debug.Log("Hey! I found:" + gameObject.name);
             ARmanager.Instance.SomeMarkerMessage(gameObject.name);
+            btnEnabled = true;
+            particlerainButton.SetActive(false);
+            btnGo.SetActive(btnEnabled);
+            if (buttonWasClicked == true)
+            {
+                particlerainButton.SetActive(true);
+
+                buttonWasClicked = false;
+            }
         }
+        else
+        {
+            btnEnabled = false;
+            btnGo.SetActive(btnEnabled);
+            particlerainButton.SetActive(false);
+            occlusionPlane.transform.position = new Vector3(-0.03f, 0.074f, 0.198f);
+        }
+        
+        
     }
 	// Use this for initialization
 	void Start () {
@@ -24,10 +47,23 @@ public class MarkerDetected : MonoBehaviour, ITrackableEventHandler {
             myBehaviour.RegisterTrackableEventHandler(this);
            
         }
+        occlusionPlane.transform.position = new Vector3(-0.03f, 0.074f, 0.198f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (buttonWasClicked == true)
+        {
+            particlerainButton.SetActive(true);
+
+            buttonWasClicked = false;
+        }
+    }
+    public void onButtonClick() {
+        
+        occlusionPlane.transform.position = new Vector3(-0.03f, -0.001f, 0.198f);
+        btnEnabled = false;
+        buttonWasClicked = true;
+        btnGo.SetActive(btnEnabled);
+    }
 }
---
+++ Assets/Resources/particleRain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class particleRain : MonoBehaviour {
+    public ParticleSystem particlerain;
+    private TrackableBehaviour myBehaviour;
+  
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick() {
+        particlerain.Play();
+    }
+}
